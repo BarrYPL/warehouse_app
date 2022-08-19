6b5846dfd39376df7b1e807c375f780d91176ba2
BarrYPL <obartek4@gmail.com>
2022-08-20 00:16:08 +0200

    Added 2 eastereggs.

--- public/js/general-js.js
+++ public/js/general-js.js
@@ -1,19 +1,40 @@
 "use strict";
 
+var pedoCounter = 0;
+
 function _(el){
  return document.getElementById(el);
 }
 
 function showMenu(){
   const menuButton = $('#menu-icon');
-  console.log("show menu");
+  pedoCounter++;
+  if (pedoCounter >= 3){
+    callForPedobear();
+  }
   _('menu-div').style.transform = 'rotate(-15deg)';
   menuButton.attr('onclick', 'hideMenu()');
 }
 
 function hideMenu(){
   const menuButton = $('#menu-icon');
-  console.log("hide menu");
   _('menu-div').style.transform = 'rotate(0deg)';
+  _('pedoEasterEgg').style.transform = 'rotate(0deg)';
+  _('micheEasterEgg').style.transform = 'rotate(0deg)';
   menuButton.attr('onclick', 'showMenu()');
+}
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+function callForPedobear(){
+  _('pedoEasterEgg').style.transform = 'rotate(50deg)';
+  _('second-egg').style.display = 'block';
+  _('second-egg').style.top = '80vh';
+}
+
+function showMiche(){
+  _('pedoEasterEgg').style.transform = 'rotate(0deg)';
+  _('micheEasterEgg').style.transform = 'rotate(80deg)';
 }
