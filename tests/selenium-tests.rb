869ced71ea52d60469f0a44e0bf631a05889c73c
BarrYPL <obartek4@gmail.com>
2022-09-26 00:38:46 +0200

    First try of selenium tests.

--- tests/selenium-tests.rb
+++ tests/selenium-tests.rb
@@ -3,19 +3,18 @@ require 'test-unit'
 class AutomaticTest < Test::Unit::TestCase
 
 def setup
-  @driver = Selenium::WebDriver.for :chrome
+  @driver = Selenium::WebDriver.for :firefox
   @url = "localhost:4567"
-  @driver.manage.timeouts.implicit_wait = 30
+  @driver.manage.timeouts.implicit_wait = 30;
  end
 
 def test_main_page
   @driver.get(@url)
-# Search for the search box using the name selector
   @query_box = @driver.find_element(:name, 'search-input')
-# Input the search phrase we need to search for in the searched query_box element
   @query_box.send_keys("11")
-# Click submit button(enter to search)
   @query_box.submit
+  @myDynamicElement = @driver.find_element(:id, 'results-count')
+  p @myDynamicElement.text
  end
 
 def teardown
